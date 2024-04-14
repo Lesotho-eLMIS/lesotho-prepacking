3507c65776430722120d7913173cfda55f90af5b
ksetetemela <khobatha.setetemela@gmail.com>
2024-04-14 06:37:47 +0200

    add missing import

--- src/main/java/org/openlmis/prepacking/domain/event/PrepackingEventLineItem.java
+++ src/main/java/org/openlmis/prepacking/domain/event/PrepackingEventLineItem.java
@@ -16,6 +16,7 @@
 package org.openlmis.prepacking.domain.event;
 
 import java.util.UUID;
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Table;
 import lombok.AllArgsConstructor;
