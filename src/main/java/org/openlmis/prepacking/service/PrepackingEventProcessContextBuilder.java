75f8a156cb875f6bad84de04f08363984b3f04a9
ksetetemela <khobatha.setetemela@gmail.com>
2024-04-19 13:08:10 +0200

    remove unused imports

--- src/main/java/org/openlmis/prepacking/service/PrepackingEventProcessContextBuilder.java
+++ src/main/java/org/openlmis/prepacking/service/PrepackingEventProcessContextBuilder.java
@@ -35,8 +35,6 @@ import org.slf4j.Logger;
 import org.slf4j.ext.XLogger;
 import org.slf4j.profiler.Profiler;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.oauth2.provider.OAuth2Authentication;
 import org.springframework.stereotype.Service;
 
 /**
